8007f53811446a4fbe6c20baa773ada3bbffaa1d
Michael <michaelslugin13@gmail.com>
2024-03-05 23:19:09 -0800

    Tests are ready
    Tests scripts for getMonthName func.

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-export function sample_sum (a, b) {
-    return a + b;
-}
+export const getMonthName = (monthNumber) => {
+    const inputDataMistake = 'Ошибка задания входных данных';
+    const inputDataMistakeIndex = 0;
+    const seasonName = [
+        { 'season': 'зима', 'months': [1, 2, 12] },
+        { 'season': 'весна', 'months': [3, 4, 5] },
+        { 'season': 'лето', 'months': [6, 7, 8] },
+        { 'season': 'осень', 'months': [9, 10, 11] },
+    ];
+    const monthName = [
+        inputDataMistake,
+        'январь',
+        'февраль',
+        'март',
+        'апрель',
+        'май',
+        'июнь',
+        'июль',
+        'август',
+        'сентябрь',
+        'октябрь',
+        'ноябрь',
+        'декабрь'
+    ];
+    monthNumber = Math.floor(monthNumber);
+    let resultMonthName = monthName[inputDataMistakeIndex];
+    let resultSeasonName = inputDataMistake;
+    if ((monthNumber > inputDataMistakeIndex) && (monthNumber < monthName.length)) {
+        resultMonthName = monthName[monthNumber];
+        seasonName.forEach((seasonNameObject) => {
+            for (let i in seasonNameObject) {
+                if (i === 'months') {
+                    seasonNameObject[i].forEach((monthNumberToCompare) => {
+                        if (monthNumberToCompare === monthNumber) {
+                            resultSeasonName = seasonNameObject['season'];
+                        }
+                    })
+                }
+            }
+        });
+        return { 'monthName': resultMonthName, 'seasonName': resultSeasonName };
+    } else return inputDataMistake;
+}		    
--- index.test.js
+++ index.test.js
@@ -1,4 +1,39 @@
-import { sample_sum } from "./index.js"
-test ('addition 3 + 8 to equal 11',() => {
-    expect(sample_sum(3, 8)).toBe (11);
-})
+import { getMonthName } from "./index.js";
+describe ('проверка функции getMonthName',
+() =>
+    {
+    const errorMessage = 'Ошибка задания входных данных',
+    normalOutput3 = {'monthName':'март', 'seasonName':'весна'},
+    normalOutput8 = {'monthName':'август', 'seasonName':'лето'};
+
+     it('ввод номера месяца больше 13 (corner - success)', () =>
+     {
+        expect(getMonthName(13)).toBe(errorMessage);
+     }),
+
+     it('ввод номера месяца меньше 1 (corner - success)', () =>
+     {
+        expect(getMonthName(0)).toBe(errorMessage);
+     }),
+
+     it ('номер месяца 3 (success)', () =>
+     {
+        expect(getMonthName(3)).toStrictEqual(normalOutput3);
+     }),
+
+     it ('номер месяца 2 (fail)', () =>
+     {
+        expect(getMonthName(2)).toStrictEqual(normalOutput3);
+     }),
+
+     
+     it ('номер месяца 8.5 (corner - success)', () =>
+     {
+        expect(getMonthName(8.5)).toStrictEqual(normalOutput8);
+     }),
+
+     it ('номер месяца -.89 (corner - fail)', () =>
+     {
+        expect(getMonthName(-.89)).toStrictEqual(normalOutput8);
+     })
+    })
